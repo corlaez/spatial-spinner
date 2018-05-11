b7351979d02980c93113792b3927b20d9548b1aa
J Armando Cordova <armanpela@gmail.com>
2018-05-10 22:31:26 -0700

    New Player Sprites

--- src/components/Bunny.js
+++ src/components/Bunny.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Sprite } from "react-pixi-fiber";
 import * as PIXI from "pixi.js";
-//import bunny from "./player.png";
+import bunny from "./player.png";
+import p2 from "./player2.png";
 
 const centerAnchor = new PIXI.Point(0.5, 0.5);
-const bunny = "https://i.imgur.com/IaUrttj.png";
+//const bunny = "https://i.imgur.com/IaUrttj.png";
 
 function Bunny(props) {
   return (
     <Sprite
       anchor={centerAnchor}
-      texture={PIXI.Texture.fromImage(bunny)}
+      texture={PIXI.Texture.fromImage(p2)}
       {...props}
     />
   );
