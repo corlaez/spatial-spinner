0437ea1591593c188d958a673f7a66ca14414be0
J Armando Cordova <armanpela@gmail.com>
2018-05-12 14:26:50 -0700

    Static star background

--- src/components/Stars.js
+++ src/components/Stars.js
@@ -1,29 +1,37 @@
 import React from "react";
 import { connect } from "@cerebral/react"
-import { state } from "cerebral/tags"
+import { state, signal } from "cerebral/tags"
 import stark1 from './stark1.png'
 import stark2 from './stark2.png'
 import stark3 from './stark3.png'
 import stark4 from './stark4.png'
 import { Text } from "react-pixi-fiber";
 import playerDirection from "../computed/playerDirection";
+import ImageSprite from "./ImageSprite";
 
 class Stars extends React.Component {
+    componentDidMount() {
+        this.props.init();
+    }
+    
     render() {
-        return (
-            <Text 
-                x={20} 
-                y={20} 
-                style={{color: 'white'}} 
-                text={JSON.stringify(this.props.direction)}
+        return this.props.stars.map(star =>
+            <ImageSprite
+                key={star.key}
+                image={stark1}  
+                x={star.x}
+                y={star.y}
+                scale={star.scale}
             />
         )
     }
+
 }
 
 export default connect(
     {
-        direction: playerDirection
+        stars: state`app.background.stars`,
+        init: signal`app.background.init`,
     },
     Stars
 );
